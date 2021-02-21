07fc2559f5cd9c4bfa6d182da267fa8f45da8a9e
AndroidAde <adesanyaaa@gmail.com>
2021-02-21 01:01:07 +0000

    Initial commit

---
+++ app/src/main/java/com/adesoftware/fetchukcoviddata/FetchUKCoVidDataActivity.kt
@@ -0,0 +1,11 @@
+package com.adesoftware.fetchukcoviddata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FetchUKCoVidDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fetch_uk_covid_data)
+    }
+}
